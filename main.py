e32a6437e0602e444327b825b25591102cf9ed8f
catminespecial414 <xdzhinmi@outlook.com>
2026-03-16 18:56:25 +0800

    添加main.py,可以启动网页和数据库

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# main.py
+from network import app
+from database import init_db
+
+if __name__ == "__main__":
+    # 初始化数据库
+    init_db()
+    print("✅ Database initialized")
+
+    # 启动 Flask 服务器
+    app.run(
+        host="0.0.0.0",    # 局域网可访问
+        port=5000,
+        debug=True          # 开发调试模式
+    )
